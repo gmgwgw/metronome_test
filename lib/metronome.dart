0a621ab5b4153ec72f92c3f24f329e18817b3ac1
gmgwgw <gorilla_a3@outlook.com>
2024-02-06 23:40:21 +0900

    音声再生のテスト

--- lib/metronome.dart
+++ lib/metronome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 import 'dart:async';
 
 @override
@@ -13,14 +14,25 @@ class Metronome extends StatefulWidget {
 
 class _MetronomeState extends State<Metronome> {
   int _time = 1;
+  final _player = AudioPlayer();
 
   @override
   void initState() {
     Timer.periodic(
-      const Duration(milliseconds: 666),
+      const Duration(milliseconds: 300),
       _onTimer,
     );
+    _init();
     super.initState();
+  }
+
+  Future<void> _init() async {
+    try {
+      await _player.setAsset('assets/pi.mp3');
+    } catch (e) {
+      // エラー処理
+      print("An error occurred: ${e.toString()}");
+    }
   }
 
   void _onTimer(Timer timer) {
@@ -30,6 +42,8 @@ class _MetronomeState extends State<Metronome> {
     } else {
       nextTime = _time + 1;
     }
+    Timer.run(() => _player.play());
+    Timer.run(() => _player.seek(const Duration(seconds: 0)));
     setState(() => _time = nextTime);
   }
 
